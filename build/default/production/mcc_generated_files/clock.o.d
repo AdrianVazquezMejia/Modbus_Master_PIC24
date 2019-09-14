feae0945b6b5ea761e3cde089e9cf3a9dc665cff
AdrianVazquezMejia <aavm0000@gmail.com>
2019-09-14 11:00:26 -0400

    Reception changed from Slave to Master Mode, some clean up

--- build/default/production/mcc_generated_files/clock.o.d
+++ build/default/production/mcc_generated_files/clock.o.d
@@ -1,3 +1,2 @@
-build/default/production/mcc_generated_files/clock.o:  \
- mcc_generated_files/clock.c  \
-mcc_generated_files/clock.h 
+build/default/production/mcc_generated_files/clock.o: \
+ mcc_generated_files/clock.c mcc_generated_files/clock.h
